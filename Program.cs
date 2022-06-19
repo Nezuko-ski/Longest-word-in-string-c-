9f5f5ea9b218112e4e4754ae8b96db0302215eb4
Jonathan EDEBE <sheezgatgreatass@gmail.com>
2022-06-19 12:03:41 +0100

    git commit -m "Find longest word"

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nezuko
+{
+    public class Program
+    {
+
+        public static string LocateShort(string str)
+        {
+            return string.Concat(str.Split().OrderByDescending(v => v.Length).First());
+
+        }
+        public static void Main()
+        {
+            Console.WriteLine(LocateShort("She told me to call her cousin but Í'm not feeling like it."));
+        }
+    }
+}
